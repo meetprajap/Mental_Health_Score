ba98fcb3cf30cca723556de53c953f440582cf93
meetprajap <meetp5986@gmail.com>
2026-08-03 02:10:09 +0530

    deployment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (() => {
   "use strict";
 
-//   const API_BASE = "https://mansik-santulan-score.onrender.com";
-  const API_BASE = "http://127.0.0.1:2200";
+
+  const API_BASE = "https://mental-health-score-49ru.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
